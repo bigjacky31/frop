8e8c0310a4ef5314f1992346ef715f4bd7a63668
Mathias <Mathias@salon>
2014-07-16 20:59:07 +0200

    Init of business rules of calculation of the kilometers rate

--- frop/js/index.js
+++ frop/js/index.js
@@ -1,3 +1,55 @@
+function FropFormCtrl($scope) {
+
+	$scope.calculateKilometersCost = function( fiscalPower, kilometers ){
+		//Bareme 2014
+		// cf http://www.service-public.fr/actualites/003037.html
+		if( fiscalPower <= 3 ){
+			if( kilometers <= 5000 ){ return kilometers * 0.408; }
+			else if( kilometers <= 20000 ){ return kilometers * 0.244 + 820; }
+			else{ return kilometers * 0.285; }
+		}
+		else if( fiscalPower == 4 ){
+			if( kilometers <= 5000 ){ return kilometers * 0.491; }
+			else if( kilometers <= 20000 ){ return kilometers * 0.276 + 1077; }
+			else{ return kilometers * 0.330; }
+		}
+		else if( fiscalPower == 5 ){
+			if( kilometers <= 5000 ){ return kilometers * 0.540; }
+			else if( kilometers <= 20000 ){ return kilometers * 0.303 + 1182; }
+			else{ return kilometers * 0.362; }
+		}
+		else if( fiscalPower == 6 ){
+			if( kilometers <= 5000 ){ return kilometers * 0.565; }
+			else if( kilometers <= 20000 ){ return kilometers * 0.318 + 1238; }
+			else{ return kilometers * 0.380; }
+		}
+		else{
+			if( kilometers <= 5000 ){ return kilometers * 0.592; }
+			else if( kilometers <= 20000 ){ return kilometers * 0.335 + 1282; }
+			else{ return kilometers * 0.399; }
+		}
+	};
+
+	$scope.submit = function() {
+
+		var msg = "";
+		msg += "fropIncome : " + $scope.fropIncome + "\n";
+		msg += "fropFiscalPower : " + $scope.fropFiscalPower + "\n";
+		msg += "fropKilometers : " + $scope.fropKilometers + "\n";
+		msg += "fropFoodPrice : " + $scope.fropFoodPrice + "\n";
+
+		alert( msg );
+	};
+}
+
+
+
+
+
+
+
+
+
 $(function () {
 	$('#chart-div').highcharts({
 		chart: {
