ed2800579c21adc90b5a73fdd1fb7530305c9b07
Mathias <Mathias@salon>
2014-07-21 21:07:09 +0200

    Calculate of the rentability point by dichotomy

--- frop/js/index.js
+++ frop/js/index.js
@@ -33,7 +33,30 @@ function FropFormCtrl($scope) {
 	$scope.calculateDefaultCost = function(income){
 		return income * 0.1;
 	};
-	
+
+	$scope.calculateRentabilityKms = function( fiscalPower, income ){
+		//Calcul de la borne max
+		var defaultCost = $scope.calculateDefaultCost( income )
+		var borneMax = 100000;
+
+
+		//Calcul par dichotomie
+		var borneMin = 0;
+		var goOn = true;
+		var borneMiddle = ( borneMin + borneMax ) / 2;
+
+		while( borneMax - borneMin > 1 ){
+			if( defaultCost > $scope.calculateFraisReelsCost( fiscalPower, borneMiddle ) ){
+				borneMin = borneMiddle;
+			}
+			else{
+				borneMax = borneMiddle;
+			}
+			borneMiddle = ( borneMin + borneMax ) / 2;
+		}
+		return borneMiddle;
+	};
+
 
 	$scope.submit = function() {
 
@@ -42,14 +65,16 @@ function FropFormCtrl($scope) {
 		msg += "fropFiscalPower : " + $scope.fropFiscalPower + "\n";
 		msg += "fropKilometers : " + $scope.fropKilometers + "\n";
 		msg += "fropFoodPrice : " + $scope.fropFoodPrice + "\n";
-		
+
 		var fraisReelsCost = $scope.calculateFraisReelsCost( $scope.fropFiscalPower, $scope.fropKilometers );
 		var defaultCost = $scope.calculateDefaultCost( $scope.fropIncome );
-		
+		var rentabilityKms = $scope.calculateRentabilityKms($scope.fropFiscalPower, $scope.fropIncome);
+
 		msg += "calculateFraisReelsCost : " + fraisReelsCost + "\n";
 		msg += "calculateDefaultCost : " + defaultCost + "\n";
-		
-		
+		msg += "calculateRentabilityKms : " + rentabilityKms + "\n";
+
+
 		//TODO A FAIRE
 		if( fraisReelsCost > defaultCost ){
 			$('.well p').html('Pour vous, il est plus intéressant de passer au frais réels. Blablabla blablabla');
@@ -57,90 +82,99 @@ function FropFormCtrl($scope) {
 		else{
 			$('.well p').html('Pour vous, c\'est nul');
 		}
-		
-		
+
+
 
 		alert( msg );
+
+
+
+		$('#chart-div').highcharts({
+			chart: {
+				type: 'area'
+			},
+			title: {
+				text: 'Frais réels... Ou pas !'
+			},
+			credits: {
+				enabled: false
+			},
+			exporting: {
+				enabled: false 
+			},
+
+			xAxis: {
+				title: {
+					enabled: true,
+					text: 'Kilomètres'
+				},
+				labels: {
+					formatter: function() {
+						return this.value +' km';
+					}
+				},
+				plotLines : [
+				             {
+				            	 value : rentabilityKms,
+				            	 color : 'green',
+				            	 width : 2,
+				            	 label : {
+				            		 text : 'Rentable'
+				            	 }
+				             },
+				             {
+				            	 value : $scope.fropKilometers,
+				            	 color : 'red',
+				            	 width : 2,
+				            	 label : {
+				            		 text : 'Votre kilométrage'
+				            	 }
+				             }
+				             ],
+				             showLastLabel: true
+			},
+			yAxis: {
+				title: {
+					text: 'Déductible'
+				},
+				labels: {
+					formatter: function() {
+						return this.value + ' €';
+					}
+				},
+				lineWidth: 2
+			},
+			legend: {
+				enabled: false
+			},
+			tooltip: {
+				headerFormat: '<b>{series.name}</b><br/>',
+				pointFormat: '{point.x} km: {point.y} €'
+			},
+			plotOptions: {
+				spline: {
+					marker: {
+						enable: false
+					}
+				}
+			},
+			series: [{
+				name: '10%',
+				color: 'rgba(165,170,217,1)',
+				data: [[0, defaultCost], [30000, defaultCost]]
+			}, {
+				name: 'Frais réels',
+				color: 'rgba(126,86,134,.9)',
+				//TODO sort data
+				data: [
+				       [0, 0], 
+				       [rentabilityKms, $scope.calculateFraisReelsCost($scope.fropFiscalPower, rentabilityKms)],
+				       [5000, $scope.calculateFraisReelsCost($scope.fropFiscalPower, 5000)], 
+				       [20000, $scope.calculateFraisReelsCost($scope.fropFiscalPower, 20000)], 
+				       [30000, $scope.calculateFraisReelsCost($scope.fropFiscalPower, 30000)]
+				       
+				       ]
+			}]
+		});
 	};
 }
-
-
-
-
-
-
-
-
-
-$(function () {
-	$('#chart-div').highcharts({
-		chart: {
-			type: 'area'
-		},
-		title: {
-			text: 'Frais réels... Ou pas !'
-		},
-		credits: {
-			enabled: false
-		},
-		exporting: {
-			enabled: false 
-		},
-
-		xAxis: {
-			title: {
-				enabled: true,
-				text: 'Kilomètres'
-			},
-			labels: {
-				formatter: function() {
-					return this.value +' km';
-				}
-			},
-			maxPadding: 0.05,
-			plotLines : [{
-				value : 17500,
-				color : 'green',
-				width : 2,
-				label : {
-					text : 'Rentable'
-				}
-			}],
-			showLastLabel: true
-		},
-		yAxis: {
-			title: {
-				text: 'Déductible'
-			},
-			labels: {
-				formatter: function() {
-					return this.value + ' €';
-				}
-			},
-			lineWidth: 2
-		},
-		legend: {
-			enabled: false
-		},
-		tooltip: {
-			headerFormat: '<b>{series.name}</b><br/>',
-			pointFormat: '{point.x} km: {point.y} €'
-		},
-		plotOptions: {
-			spline: {
-				marker: {
-					enable: false
-				}
-			}
-		},
-		series: [{
-			name: '10%',
-			color: 'rgba(165,170,217,1)',
-			data: [[0, 1800], [25000, 1800]]
-		}, {
-			name: 'Frais réels',
-			color: 'rgba(126,86,134,.9)',
-			data: [[0, 0], [5000, 1000], [17500, 1800], [20000, 2000], [25000, 3000]]
-		}]
-	});
-});
