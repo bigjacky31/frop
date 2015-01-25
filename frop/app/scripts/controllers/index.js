c8c52dbc03acee59fb4e1775f82f4e1c0a1ca437
Mathias <Mathias@salon>
2015-01-25 20:08:52 +0100

    Industrialisation (npm + bower + grunt)

---
+++ frop/app/scripts/controllers/index.js
@@ -0,0 +1,210 @@
+'use strict';
+
+angular.module('titiApp')
+  .controller('FropFormCtrl', function ($scope) {
+
+		$scope.calculateFraisReelsCost = function( fiscalPower, kilometers ){
+			//Bareme 2014
+			// cf http://www.service-public.fr/actualites/003037.html
+			if( fiscalPower <= 3 ){
+				if( kilometers <= 5000 ){ return kilometers * 0.408; }
+				else if( kilometers <= 20000 ){ return kilometers * 0.244 + 820; }
+				else{ return kilometers * 0.285; }
+			}
+			else if( fiscalPower == 4 ){
+				if( kilometers <= 5000 ){ return kilometers * 0.491; }
+				else if( kilometers <= 20000 ){ return kilometers * 0.276 + 1077; }
+				else{ return kilometers * 0.330; }
+			}
+			else if( fiscalPower == 5 ){
+				if( kilometers <= 5000 ){ return kilometers * 0.540; }
+				else if( kilometers <= 20000 ){ return kilometers * 0.303 + 1182; }
+				else{ return kilometers * 0.362; }
+			}
+			else if( fiscalPower == 6 ){
+				if( kilometers <= 5000 ){ return kilometers * 0.565; }
+				else if( kilometers <= 20000 ){ return kilometers * 0.318 + 1238; }
+				else{ return kilometers * 0.380; }
+			}
+			else{
+				if( kilometers <= 5000 ){ return kilometers * 0.592; }
+				else if( kilometers <= 20000 ){ return kilometers * 0.335 + 1282; }
+				else{ return kilometers * 0.399; }
+			}
+		};
+
+		$scope.calculateDefaultCost = function(income){
+			return income * 0.1;
+		};
+
+		$scope.calculateRentabilityKms = function( fiscalPower, income ){
+			//Calcul de la borne max
+			var defaultCost = $scope.calculateDefaultCost( income )
+			var borneMax = 100000;
+
+
+			//Calcul par dichotomie
+			var borneMin = 0;
+			var goOn = true;
+			var borneMiddle = ( borneMin + borneMax ) / 2;
+
+			while( borneMax - borneMin > 1 ){
+				if( defaultCost > $scope.calculateFraisReelsCost( fiscalPower, borneMiddle ) ){
+					borneMin = borneMiddle;
+				}
+				else{
+					borneMax = borneMiddle;
+				}
+				borneMiddle = ( borneMin + borneMax ) / 2;
+			}
+			return borneMiddle;
+		};
+
+		
+
+		$scope.sortSerie = function( serie ){
+			var sortedSerie = [];
+			
+			for( var i = 0 ; i < serie.length ; i++ ){
+				var curPoint = serie[i];
+				var goOn = true;
+				for( var j = 0 ; goOn && j < sortedSerie.length ; j++ ){
+					var curSortedPoint = sortedSerie[j];
+					if( curPoint[0] < curSortedPoint[0] ){
+						sortedSerie.splice(j, 0, curPoint);
+						goOn = false;
+					}
+				}
+				if( goOn ){
+					sortedSerie.push(curPoint);
+				}
+			}
+			
+			return sortedSerie;
+		}
+		
+
+		$scope.submit = function() {
+
+			var msg = "";
+			msg += "fropIncome : " + $scope.fropIncome + "\n";
+			msg += "fropFiscalPower : " + $scope.fropFiscalPower + "\n";
+			msg += "fropKilometers : " + $scope.fropKilometers + "\n";
+			msg += "fropFoodPrice : " + $scope.fropFoodPrice + "\n";
+
+			var fraisReelsCost = $scope.calculateFraisReelsCost( $scope.fropFiscalPower, $scope.fropKilometers );
+			var defaultCost = $scope.calculateDefaultCost( $scope.fropIncome );
+			var rentabilityKms = $scope.calculateRentabilityKms($scope.fropFiscalPower, $scope.fropIncome);
+
+			msg += "calculateFraisReelsCost : " + fraisReelsCost + "\n";
+			msg += "calculateDefaultCost : " + defaultCost + "\n";
+			msg += "calculateRentabilityKms : " + rentabilityKms + "\n";
+
+
+			//TODO A FAIRE
+			if( fraisReelsCost > defaultCost ){
+				$('#result-sentence').html('<strong>Pour vous, il est plus intéressant de passer au frais réels. Blablabla blablabla</strong>');
+			}
+			else{
+				$('#result-sentence').html('<strong>Pour vous, c\'est nul</strong>');
+			}
+
+
+			//Calcul des points à afficher pour la courbe des frais réels
+			var fraisReelsSerieData = [
+			       [0, 0], 
+			       [rentabilityKms, $scope.calculateFraisReelsCost($scope.fropFiscalPower, rentabilityKms)],
+			       [5000, $scope.calculateFraisReelsCost($scope.fropFiscalPower, 5000)], 
+			       [20000, $scope.calculateFraisReelsCost($scope.fropFiscalPower, 20000)], 
+			       [30000, $scope.calculateFraisReelsCost($scope.fropFiscalPower, 30000)]
+			];
+			
+			//Tri des points à afficher pour la courbe des frais réels 
+			fraisReelsSerieData = $scope.sortSerie( fraisReelsSerieData );
+
+			alert( msg );
+
+
+
+			$('#chart-div').highcharts({
+				chart: {
+					type: 'area'
+				},
+				title: {
+					text: 'Frais réels... Ou pas !'
+				},
+				credits: {
+					enabled: false
+				},
+				exporting: {
+					enabled: false 
+				},
+
+				xAxis: {
+					title: {
+						enabled: true,
+						text: 'Kilomètres'
+					},
+					labels: {
+						formatter: function() {
+							return this.value +' km';
+						}
+					},
+					plotLines : [
+					             {
+					            	 value : rentabilityKms,
+					            	 color : 'green',
+					            	 width : 2,
+					            	 label : {
+					            		 text : 'Rentable'
+					            	 }
+					             },
+					             {
+					            	 value : $scope.fropKilometers,
+					            	 color : 'red',
+					            	 width : 2,
+					            	 label : {
+					            		 text : 'Votre kilométrage'
+					            	 }
+					             }
+					             ],
+					             showLastLabel: true
+				},
+				yAxis: {
+					title: {
+						text: 'Déductible'
+					},
+					labels: {
+						formatter: function() {
+							return this.value + ' €';
+						}
+					},
+					lineWidth: 2
+				},
+				legend: {
+					enabled: false
+				},
+				tooltip: {
+					headerFormat: '<b>{series.name}</b><br/>',
+					pointFormat: '{point.x} km: {point.y} €'
+				},
+				plotOptions: {
+					spline: {
+						marker: {
+							enable: false
+						}
+					}
+				},
+				series: [{
+					name: '10%',
+					color: 'rgba(165,170,217,1)',
+					data: [[0, defaultCost], [30000, defaultCost]]
+				}, {
+					name: 'Frais réels',
+					color: 'rgba(126,86,134,.9)',
+					//TODO sort data
+					data : fraisReelsSerieData
+				}]
+			});
+		};
+});
