f729c9d53cb54a199cee21a85a736eefbf5c1e94
Mathias <Mathias@salon>
2015-02-10 06:53:05 +0100

    references #2 Activation ou non du bouton de soumission du form

--- frop/app/scripts/controllers/frop-form.js
+++ frop/app/scripts/controllers/frop-form.js
@@ -2,9 +2,15 @@
 
 angular.module('frop').controller('FropFormCtrl', function ($scope, fropCalculator, fropInputDataStore, fiscalPowerList) {
 
-	$scope.model = fropInputDataStore.getFropInputData();
+	$scope.model = {
+			
+	}
 	$scope.fiscalPowerList = fiscalPowerList;
 
+	$scope.isSubmitEnabled = function(){
+		return ! $scope.fropForm.$pristine || $scope.fropForm.$invalid;
+	}
+	
 	$scope.submit = function() {
 		fropInputDataStore.setFropInputData( $scope.model );
 	};
